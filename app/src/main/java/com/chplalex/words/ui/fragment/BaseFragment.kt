a3d86b0bd71803737a9a31a0b750f7834047549f
chplalex <chepel.alexander@gmail.com>
2021-02-17 20:56:38 +0300

    Intermediate storage

--- app/src/main/java/com/chplalex/words/ui/fragment/BaseFragment.kt
+++ app/src/main/java/com/chplalex/words/ui/fragment/BaseFragment.kt
@@ -1,12 +1,14 @@
 package com.chplalex.words.ui.fragment
 
 import android.os.Bundle
+import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import com.chplalex.words.mvp.contract.Presenter
 import com.chplalex.words.mvp.contract.View
 import com.chplalex.words.mvp.model.AppState
 
-abstract class BaseFragment<T: AppState> : Fragment(), View {
+abstract class BaseFragment<T: AppState>(@LayoutRes contentLayoutId: Int) : Fragment(contentLayoutId), View {
+
     protected lateinit var presenter: Presenter<T, View>
 
     protected abstract fun createPresenter(): Presenter<T, View>
